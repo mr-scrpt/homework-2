42181e55550629f84ffff2fa1d2e72ba498f2913
mr.scrpt <mr.scrpt@gmail.com>
2019-07-31 17:57:42 +0300

    tests completed

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,1 +1,53 @@
-export { default } from './Chat';
+import React, {Component} from 'react';
+import Message from '../Message';
+
+
+export default class Chat extends Component {
+  state = {
+    messages: [],
+    messageInput: ''
+  };
+  changeInputMessage = (e) => {
+    const msg = e.target.value;
+    this.setState({messageInput: msg});
+  };
+  
+
+  sendMessageOnEnter = (e) => {
+    if (e.key === 'Enter'){
+      this.setState((state) => {
+          state.messages.push({text: state.messageInput});
+      });
+      this.setState({
+        messageInput: ''
+      });
+    }
+  };
+
+  render() {
+    const {messages} = this.state;
+
+    const list = messages.map(item=>{
+      return <Message text={item.text} />
+    });
+    const content = messages.length ? list : null;
+
+    return (
+      <div className="chat">
+        <ul className="list">
+          {content}
+        </ul>
+        <input type="text"
+               className='input-message'
+               onChange={this.changeInputMessage}
+               onKeyPress={this.sendMessageOnEnter}
+               value={this.state.messageInput}
+               placeholder='What needs to be done'
+        />
+      </div>
+    )
+  }
+
+
+};
+
--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,1 +1,12 @@
-export { default } from './Message';
+import React, {Component} from 'react';
+
+const Message = (props) => {
+  const {text} = props;
+  return (
+    <li>
+      {text}
+    </li>
+  )
+
+};
+export default Message
